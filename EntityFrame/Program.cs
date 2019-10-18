e77f1412348b70c3f3d2d7f4ff19a86659fa061a
krocden <kevin.hong715@gmail.com>
2019-10-17 23:54:19 -0400

    Add project files.

---
+++ EntityFrame/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFramework;
+
+namespace EntityFrame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test Code Here. Test Methods
+
+            // Add Methods
+            // One record for each table.
+            var food = new Food();
+            food.name = "Spaghetti";
+            food.size = "40*60";
+            food.active = true;
+            myDb.AddFood(food);
+
+            var ingredient = new Ingredient();
+            ingredient.name = "Pasta";
+            ingredient.size = "4*130";
+            ingredient.weight = "60g";
+            ingredient.active = true;
+            myDb.AddIngredient(ingredient);
+
+            var product = new Product();
+            product.foodId = food.foodId;
+            product.ingredientId = ingredient.ingredientId;
+            product.productionDate = DateTime.Today;
+            myDb.AddProduct(product);
+
+            // Edit Method
+            var ingredient2 = new Ingredient();
+            ingredient2.weight = "75g";
+            myDb.EditIngredient(ingredient.ingredientId, ingredient2);
+
+            //Display all
+            var ProductList = myDb.GetProducts();
+
+            foreach (Product p in ProductList)
+            {
+                Food foodD = myDb.GetFood(p.foodId);
+                Ingredient ingredientD = myDb.GetIngredient(p.ingredientId);
+                Console.WriteLine("Food name = "+foodD.name+" and Ingredient = "+ingredientD.name);
+            }
+        }
+    }
+}
---
+++ EntityFramework/myDb.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+    public class myDb
+    {
+        //Entity Framework Object 
+        private static FoodEntities db = new FoodEntities();
+
+        // table Product has 2 foreign key with table Ingredient and table Food.
+
+        // Enums for full list.
+        public static IEnumerable<Food> GetFoods()
+        {
+            return db.Foods.ToList();
+        }
+        public static IEnumerable<Ingredient> GetIngredient()
+        {
+            return db.Ingredients.ToList();
+        }
+        public static IEnumerable<Product> GetProducts()
+        {
+            return db.Products.ToList();
+        }
+
+        // Data retrieval Methods for table Food, Ingredient and Product
+        public static Food GetFood(int x)
+        {
+            return db.Foods.Find(x);
+        }
+        public static Ingredient GetIngredient(int x)
+        {
+            return db.Ingredients.Find(x);
+        }
+        public static Product GetProduct(int x)
+        {
+            return db.Products.Find(x);
+        }
+
+        //Add Methods 
+        public static void AddFood(Food x)
+        {
+            db.Foods.Add(x);
+            db.SaveChanges();
+        }
+        public static void AddIngredient(Ingredient x)
+        {
+            db.Ingredients.Add(x);
+            db.SaveChanges();
+        }
+        public static void AddProduct(Product x)
+        {
+            db.Products.Add(x);
+            db.SaveChanges();
+        }
+
+        // Edit method
+        public static void EditFood(int id, Food x)
+        {
+            db.Foods.Find(id).name = x.name;
+            db.Foods.Find(id).size = x.size;
+            db.SaveChanges();
+        }
+        public static void EditIngredient(int id, Ingredient x)
+        {
+            db.Ingredients.Find(id).name = x.name;
+            db.Ingredients.Find(id).size = x.size;
+            db.Ingredients.Find(id).weight = x.weight;
+            db.SaveChanges();
+        }
+        public static void EditProduct(int id, Product x)
+        {
+            db.Products.Find(id).productionDate = x.productionDate;
+            db.SaveChanges();
+        }
+
+        // Delete methods (Set to non active)
+        public void DeleteFood(int x)
+        {
+            Food food = db.Foods.Find(x);
+            db.Foods.Find(x).active = false;
+            db.SaveChanges();
+        }
+        public void DeleteIngredient(int x)
+        {
+            Ingredient ingredient = db.Ingredients.Find(x);
+            db.Ingredients.Find(x).active = false;
+            db.SaveChanges();
+        }
+        public void DeleteProduct(int x)
+        {
+            Product product = db.Products.Find(x);
+            db.Products.Find(x).active = false;
+            db.SaveChanges();
+        }
+  
+    }
+}
